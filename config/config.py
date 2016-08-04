da8b6b457b1bb2d67790cff6a43c6582dd7c6c11
barryz <barryzxb@gmail.com>
2016-08-04 22:12:16 +0800

    去敏感信息

--- config/config.py
+++ config/config.py
@@ -7,14 +7,11 @@ PASSWORD="admin"
 PORT=15672
 STEP=10
 
-'''
 try:
     import socket
     HOSTNAME=socket.gethostname()
 except Exception:
     import commands
     HOSTNAME=commands.getoutput("hostname")
-'''
-HOSTNAME = "xg-fanout-rmq-1"
 
 FALCON_API = "http://127.0.0.1:1988/v1/push"
